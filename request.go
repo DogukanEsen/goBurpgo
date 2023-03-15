bd318162c59295101773689b08a9512cca6dc437
Satı Doğukan ESEN <esendogukan@hotmail.com>
2023-03-15 09:36:01 +0300

    new feauteres

--- request.go
+++ request.go
@@ -3,8 +3,20 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"os"
+	"time"
 )
+
+type RequestData struct {
+	RequestNumber  int
+	RequestPayload string
+	RequestStatus  string
+	RequestLength  int
+	RequestTime    float64
+}
 
 func check(e error) {
 	if e != nil {
@@ -26,21 +38,46 @@ func kelimeOku(filename string) []string {
 	return words
 }
 
-func main() {
-	fmt.Printf("Welcome to app. Please enter your wordlist name: ")
-	var wordlistName string
-	fmt.Scan(&wordlistName)
-	var wordlist []string = kelimeOku(wordlistName)
-	fmt.Printf("%s is reading....", wordlistName)
-	fmt.Println(wordlist)
+func singleValue(valueName string, wordlist []string) {
 
-	/*
-		data := url.Values{"name": {"John Doe"}, "aa": {"aa"}}
-		r, err := http.PostForm("https://echoof.me", data)
+	fmt.Println("--- Requests -|---------- Payload ----------|- Status -|- Length -|- Time ---")
+	for request, a := range wordlist {
+		Start := time.Now()
+		r, err := http.PostForm("https://echoof.me", url.Values{valueName: {a}})
 		check(err)
+		ava := time.Since(Start).Seconds()
 		defer r.Body.Close()
-		io.Copy(os.Stdout, r.Body)
-		print("Deneme")
-	*/
+		b, err := io.ReadAll(r.Body)
+		check(err)
+		body := string(b)
+
+		//request
+		fmt.Printf("%d \t", request)
+		//payload
+		fmt.Printf("%s \t", a)
+		//status
+		fmt.Printf("%s \t", r.Status)
+		//length
+		a := len(body)
+		fmt.Printf("%d \t", a)
+		//Time
+		fmt.Printf("%f \n", ava)
+
+	}
+}
+
+func main() {
+	var wordlistName string
+	var valueName string
+	var wordlist []string
+
+	fmt.Printf("Welcome to app. Please enter your wordlist name: ")
+	fmt.Scan(&wordlistName)
+	wordlist = kelimeOku(wordlistName)
+	fmt.Printf("%s is reading....\n", wordlistName)
+	fmt.Printf("Please enter your value name: ")
+	fmt.Scan(&valueName)
+
+	singleValue(valueName, wordlist)
 
 }
