d73da9c8408103ec9ce408b2b6b2c6d69ea31373
Satı Doğukan ESEN <esendogukan@hotmail.com>
2023-04-26 00:32:53 +0300

    numbers payload added

--- request.go
+++ request.go
@@ -7,9 +7,11 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 	"time"
 )
 
+// Sıralama yapma, max 3'e kadar payload gönderme, istenilen yeri değiştirme, hazır wordlistler
 type RequestData struct {
 	RequestNumber  int
 	RequestPayload string
@@ -19,6 +21,7 @@ type RequestData struct {
 }
 
 var structMap = make(map[int]interface{})
+var tempWordlist []string
 
 func check(e error) bool {
 	err := false
@@ -45,11 +48,11 @@ func kelimeOku(filename string) []string {
 	return words
 }
 
-func singleValue(valueName string, wordlist []string) {
+func singleValue(valueName string, wordlist []string, website string) {
 	fmt.Println("--- Requests -|---------- Payload ----------|- Status -|- Length -|- Time ---")
 	for request, payload := range wordlist {
 		Start := time.Now()
-		r, err := http.PostForm("https://echoof.me", url.Values{valueName: {payload}})
+		r, err := http.PostForm(website, url.Values{valueName: {payload}})
 		check(err)
 		totalTime := time.Since(Start).Seconds()
 		defer r.Body.Close()
@@ -78,9 +81,37 @@ func saveMap(request int, payload string, status string, length int, time float6
 	structMap[request] = out
 }
 
+func numbers(from int, to int, step int) []int {
+	//Return ederken pointer ediyor düzelt
+	var tempNumberlist []int
+	for i := from; i < to; i = i + step {
+		tempNumberlist = append(tempNumberlist, i)
+	}
+	return tempNumberlist
+}
+
+func numbers2(basamak int, from int, to int, step int) {
+	var tempNumberlist []int
+
+	switch basamak {
+	case 1, 2, 3, 4, 5, 6, 7, 8, 9:
+		for i := 0; i < 10; i++ {
+			if basamak > 1 {
+				temp := fmt.Sprintf("%0*d", basamak-1, i)
+				temp2, err := strconv.Atoi(temp)
+				check(err)
+				tempNumberlist = append(tempNumberlist, temp2)
+			} else {
+				tempNumberlist = append(tempNumberlist, i)
+			}
+		}
+	}
+}
+
 func main() {
 	var wordlistName string
 	var valueName string
+	var website string
 	var wordlist []string
 
 	fmt.Printf("Welcome to app. Please enter your wordlist name: ")
@@ -96,9 +127,14 @@ func main() {
 		}
 	}
 	fmt.Printf("%s is reading....\n", wordlistName)
+	fmt.Printf("Please enter your web link(https://echoof.me as 0): ")
+	fmt.Scan(&website)
+	if website == "0" {
+		website = "https://echoof.me"
+	}
 	fmt.Printf("Please enter your value name: ")
 	fmt.Scan(&valueName)
 
-	singleValue(valueName, wordlist)
+	singleValue(valueName, wordlist, website)
 
 }
